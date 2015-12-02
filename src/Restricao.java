34812d8333499267c4ceff162dbaf64cf72fb017
Caio Vinicius Carvalho de Cerqueira <caio_vinicius89@hotmail.com>
2015-12-02 08:19:13 -0300

    Ok
    here it goes...

---
+++ src/Restricao.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Restricao {
+	private Double limite;
+	private ArrayList<Double> variaveis = new ArrayList<Double>();
+	
+	public void addVariavel(Double valor){
+		this.variaveis.add(valor);
+	}
+	
+	public Double getLista(int indicie){
+		return this.variaveis.get(indicie);
+	}
+	
+	public void setLimite(Double valor){
+		this.limite = valor;
+	}
+	
+	public Double getLimite(){
+		return this.limite;
+	}
+}
